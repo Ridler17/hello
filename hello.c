4790c5c970852fa44a29ddc4b6020d4bd42409e4
Zachary Ridl <zach.ridl@yahoo.com>
2021-08-26 13:53:30 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,15 @@
+/**
+ * Author: Zachary Ridl
+ *          zridl2@huskers.unl.edu
+ * Date: 2021/08/26
+ * 
+ */
+ #include <stdlib.h>
+ #include <stdio.h>
+ 
+ int main(int argc, char **argv) {
+   
+     printf("Zachary Ridl\n");
+ 
+     return 0;
+ }
